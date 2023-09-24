27ec9cc283b0721ab87bf7c503ec631faba0f259
Mourad Magdy <murad.ibrahim02@eng-st.cu.edu.eg>
2023-09-24 15:25:24 +0300

    add https

--- src/useMovies.js
+++ src/useMovies.js
@@ -15,7 +15,7 @@ export function useMovies(query) {
           //Resetting the error
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok)
